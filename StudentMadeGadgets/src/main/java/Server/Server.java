a758316401e0f9fcea5f272270206d7aafc6466d
Josef-WM <josefwm@stud.ntnu.no>
2025-11-11 16:31:53 +0100

    added a simple Server.java implementation using TCP

--- StudentMadeGadgets/src/main/java/Server/Server.java
+++ StudentMadeGadgets/src/main/java/Server/Server.java
@@ -1,7 +1,14 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
-  // SERVER
-  // Accepts clients
-  // Transmits information from client to client
+  public static void main(String[] args) throws IOException {
+    // creating a server socket on port 6767
+    ServerSocket serverSocket = new ServerSocket(6767);
+
+    // close the server socket
+    serverSocket.close();
+  }
 }
