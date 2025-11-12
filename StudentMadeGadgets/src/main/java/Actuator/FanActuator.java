fd631113b4956261365db2217aa6bf7d5b768358
Josef-WM <josefwm@stud.ntnu.no>
2025-11-12 16:55:21 +0100

    improved FanActuator-class

--- StudentMadeGadgets/src/main/java/Actuator/FanActuator.java
+++ StudentMadeGadgets/src/main/java/Actuator/FanActuator.java
@@ -3,23 +3,25 @@ package Actuator;
 import Greenhouse.Greenhouse;
 
 /**
- * Represents a fan, acts as an actuator in a greenhouse
+ * Represents a fan, acts as an actuator in a greenhouse.
  */
 public class FanActuator implements Actuator {
   private Greenhouse greenhouse;
+  private final int id;
   private boolean isOn;
   private float speed;
 
   /**
-   * constructor for the FanActuator class
+   * constructor for the FanActuator class.
    */
-  public FanActuator(boolean isOn, float speed) {
+  public FanActuator(int id, boolean isOn, float speed) {
+    this.id = id;
     this.isOn = isOn;
     this.speed = speed;
   }
 
   /**
-   * turns on the fan actuator
+   * turns on the fan actuator.
    */
   public void turnOn() {
     greenhouse.changeTemperature(greenhouse.getTemperature() - 5);
@@ -27,10 +29,17 @@ public class FanActuator implements Actuator {
   }
 
   /**
-   * turns off the fan actuator
+   * turns off the fan actuator.
    */
   public void turnOff() {
     greenhouse.changeTemperature(greenhouse.getTemperature() + 5);
     isOn = false;
-    }
+  }
+
+  /**
+   * sets the speed of the fan actuator.
+   */
+  public void setSpeed(float speed) {
+    this.speed = speed;
+  }
 }
