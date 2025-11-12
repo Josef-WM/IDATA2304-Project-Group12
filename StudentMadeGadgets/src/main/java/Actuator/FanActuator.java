53f4a451074cd1859f1033a856d7c141a53f9019
Josef-WM <josefwm@stud.ntnu.no>
2025-11-12 16:31:44 +0100

    implemented FanActuator class

--- StudentMadeGadgets/src/main/java/Actuator/FanActuator.java
+++ StudentMadeGadgets/src/main/java/Actuator/FanActuator.java
@@ -1,4 +1,31 @@
 package Actuator;
 
+/**
+ * Represents a fan, acts as an actuator in a greenhouse
+ */
 public class FanActuator {
+  private boolean isOn;
+  private float speed;
+
+  /**
+   * constructor for the FanActuator class
+   */
+  public FanActuator(boolean isOn, float speed) {
+    this.isOn = isOn;
+    this.speed = speed;
+  }
+
+  /**
+   * turns on the fan actuator
+   */
+  public void turnOn() {
+    isOn = true;
+  }
+
+  /**
+   * turns off the fan actuator
+   */
+  public void turnOff() {
+    isOn = false;
+    }
 }
