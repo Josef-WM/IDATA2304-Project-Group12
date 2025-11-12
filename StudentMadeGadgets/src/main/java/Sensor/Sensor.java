bbb57ac5740f1a0713b9b5a104e103006765717f
Kevin Pham <kevinp@stud.ntnu.no>
2025-11-12 16:36:22 +0100

    Sensor

--- StudentMadeGadgets/src/main/java/Sensor/Sensor.java
+++ StudentMadeGadgets/src/main/java/Sensor/Sensor.java
@@ -1,4 +1,12 @@
 package Sensor;
 
 public interface Sensor {
+  String getId();
+  String getType();
+  String getUnit();
+  double read();
+
+  default String format() {
+    return getType() + ": " + read() + " " + getUnit();
+  }
 }
--- StudentMadeGadgets/src/main/java/Sensor/TemperatureSensor.java
+++ StudentMadeGadgets/src/main/java/Sensor/TemperatureSensor.java
@@ -1,4 +1,29 @@
 package Sensor;
 
-public class TemperatureSensor {
+import Greenhouse.Greenhouse;
+
+public class TemperatureSensor implements Sensor {
+  private final String id;
+  private final Greenhouse greenhouse;
+
+  public TemperatureSensor(String id, Greenhouse greenhouse) {
+    this.id = id;
+    this.greenhouse = greenhouse;
+  }
+
+  @Override public String getId(){
+    return id;
+  }
+
+  @Override public String getType(){
+    return "Temperature";
+  }
+
+  @Override public String getUnit(){
+    return "Celsius";
+  }
+
+  @Override public double read(){
+    return greenhouse.getTemperature();
+  }
 }
