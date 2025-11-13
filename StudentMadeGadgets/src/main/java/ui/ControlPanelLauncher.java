a66b454054eec0545db761dd404378e48fd43701
Josef-WM <josefwm@stud.ntnu.no>
2025-11-13 16:40:21 +0100

    cleaned up in javafx code

--- StudentMadeGadgets/src/main/java/ui/ControlPanelLauncher.java
+++ StudentMadeGadgets/src/main/java/ui/ControlPanelLauncher.java
@@ -1,6 +1,14 @@
 package ui;
 
+/**
+ * Responsible for starting the control panel ui.
+ */
 public class ControlPanelLauncher {
-    public static void main(String[] args) {
-    }
+  /**
+   * The main method of the control panel ui.
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    MainWindow.main(args);
+  }
 }
--- StudentMadeGadgets/src/main/java/ui/MainWindow.java
+++ StudentMadeGadgets/src/main/java/ui/MainWindow.java
@@ -10,14 +10,26 @@ import javafx.stage.Stage;
  * Main window for the control panel UI.
  */
 public class MainWindow extends Application {
-  private Label info;
 
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane root = new BorderPane();
     Scene scene = new Scene(root);
+
+    Label info = new Label("Control Panel UI");
+
+    root.setCenter(info);
+
     stage.setScene(scene);
     stage.setTitle("SMG Control Panel GUI");
     stage.show();
+  }
+
+  /**
+   * The main method of the control panel application.
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
   }
 }
