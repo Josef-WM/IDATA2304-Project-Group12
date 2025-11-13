71369a7a8ae18f77a0d78ec69acc9e314b9fd630
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2025-11-13 21:22:34 +0100

    Added message serialization to JSON

--- StudentMadeGadgets/src/main/java/protocol/JSONHandler.java
+++ StudentMadeGadgets/src/main/java/protocol/JSONHandler.java
@@ -1,5 +1,13 @@
 package protocol;
 
+import com.google.gson.Gson;
+
 public class JSONHandler {
+
+  public static String serializeMessageToJSON(Message message) {
+    Gson gson = new Gson();
+
+    return gson.toJson(message);
+  }
 
 }
