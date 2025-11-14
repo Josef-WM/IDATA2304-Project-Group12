84252d725f9c16ee7cb028658ce2f0bbf1640770
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2025-11-14 14:58:46 +0100

    Added power to constructor in ActuatorCommand

--- StudentMadeGadgets/src/main/java/protocol/command/ActuatorData.java
+++ StudentMadeGadgets/src/main/java/protocol/command/ActuatorData.java
@@ -2,23 +2,24 @@ package protocol.command;
 
 public class ActuatorData extends Command {
   private String actuatorType;
-  private int power;
   private boolean isOn;
+  private int power;
 
-  public ActuatorData(String actuatorType, boolean isOn) {
+  public ActuatorData(String actuatorType, boolean isOn, int power) {
     this.actuatorType = actuatorType;
     this.isOn = isOn;
+    this.power = power;
   }
 
   public String getActuatorType() {
     return this.actuatorType;
   }
 
-  public int getPower() {
-    return this.power;
-  }
-
   public boolean isOn() {
     return this.isOn;
+  }
+
+  public int getPower() {
+    return this.power;
   }
 }
