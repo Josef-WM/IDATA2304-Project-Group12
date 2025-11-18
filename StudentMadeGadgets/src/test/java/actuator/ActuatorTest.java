3b352710d077c7c2905bac77afea25ad3e6e8f4c
Kevin Pham <kevinp@stud.ntnu.no>
2025-11-18 14:33:16 +0100

    added Javadoc to ActuatorTest class.

--- StudentMadeGadgets/src/test/java/actuator/ActuatorTest.java
+++ StudentMadeGadgets/src/test/java/actuator/ActuatorTest.java
@@ -1,7 +1,8 @@
 package actuator;
 
 /**
- * Test implementation of {@link Actuator} used in unit tests.
+ * Test implementation of {@link Actuator} used in unit tests, which are
+ * needed to refactor SensorNodeTest Class.
  */
 public class ActuatorTest implements Actuator {
 
@@ -9,31 +10,55 @@ public class ActuatorTest implements Actuator {
   private boolean on = false;
   private int power = 0;
 
+  /**
+   * Constructor for ActuatorTest.
+   * @param id
+   */
   public ActuatorTest(String id) {
     this.id = id;
   }
 
+  /**
+   * Returns the ID of the actuator.
+   * @return
+   */
   @Override
   public String getID() {
     return id;
   }
 
+  /**
+   * Returns the type of the actuator.
+   * @return
+   */
   @Override
   public String getType() {
     return "TestActuator";
   }
 
+  /**
+   * Returns whether the actuator is on.
+   * @return
+   */
   @Override
   public boolean isOn() {
     return on;
   }
 
+  /**
+   * Toggles the actuator on or off.
+   * @return
+   */
   @Override
   public boolean toggle() {
     on = !on;
     return on;
   }
 
+  /**
+   * Sets the power level of the actuator.
+   * @param power
+   */
   @Override
   public void setPower(int power) {
     this.power = power;
