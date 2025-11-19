41f61299842e2b8f95f6c50cc95299021468ed6f
Kevin Pham <kevinp@stud.ntnu.no>
2025-11-19 16:06:46 +0100

    added humiditysensor.

--- StudentMadeGadgets/src/main/java/sensor/HumiditySensor.java
+++ StudentMadeGadgets/src/main/java/sensor/HumiditySensor.java
@@ -1,4 +1,37 @@
 package sensor;
 
-public class HumiditySensor {
+import greenhouse.Greenhouse;
+
+public class HumiditySensor implements Sensor {
+  private String ID;
+  private final Greenhouse greenhouse;
+
+  public HumiditySensor(Greenhouse greenhouse) {
+    this.greenhouse = greenhouse;
+  }
+
+  @Override
+  public String getID() {
+    return ID;
+  }
+
+  @Override
+  public void setID(String ID) {
+    this.ID = ID;
+  }
+
+  @Override
+  public String getType() {
+    return "Humidity";
+  }
+
+  @Override
+  public String getUnit() {
+    return "Percentage";
+  }
+
+  @Override
+  public double read() {
+    return (double) greenhouse.getHumidity();
+  }
 }
---
+++ StudentMadeGadgets/src/test/java/sensor/HumiditySensorTest.java
@@ -0,0 +1,7 @@
+package sensor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumiditySensorTest {
+
+}
