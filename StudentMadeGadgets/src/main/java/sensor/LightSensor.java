65b36d0b9155bc6d1dee0d8dcd3cd3e6b8248688
Kevin Pham <kevinp@stud.ntnu.no>
2025-11-19 15:33:18 +0100

    added lightsensor

--- StudentMadeGadgets/src/main/java/sensor/LightSensor.java
+++ StudentMadeGadgets/src/main/java/sensor/LightSensor.java
@@ -1,4 +1,39 @@
 package sensor;
 
-public class LightSensor {
+
+import greenhouse.Greenhouse;
+
+public class LightSensor implements Sensor {
+  private String ID;
+  private final Greenhouse greenhouse;
+
+  public LightSensor(Greenhouse greenhouse) {
+    this.greenhouse = greenhouse;
+  }
+
+  @Override
+  public String getID() {
+    return ID;
+  }
+
+  @Override
+  public void setID(String ID) {
+    this.ID = ID;
+  }
+
+  @Override
+  public String getType() {
+    return "Light";
+  }
+
+  @Override
+  public String getUnit() {
+    return "Lux";
+  }
+
+  @Override
+  public double read() {
+    return (double)  greenhouse.getLight();
+  }
+
 }
